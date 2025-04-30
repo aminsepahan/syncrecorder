65bb996785d55e5ca06d751244d4390c70d192fe
ak <aminkeshavarzian@gmail.com>
2025-04-30 12:30:43 +0330

    Add compression video

--- app/src/main/java/com/appleader707/syncrecorder/service/save/SaveService.kt
+++ app/src/main/java/com/appleader707/syncrecorder/service/save/SaveService.kt
@@ -2,42 +2,62 @@ package com.appleader707.syncrecorder.service.save
 
 import com.appleader707.syncrecorder.TAG
 import com.appleader707.syncrecorder.business.usecase.convert.EmbedSubtitleIntoVideoUseCase
+import com.appleader707.syncrecorder.business.usecase.directory.GetSyncRecorderDirectoryUseCase
 import com.appleader707.syncrecorder.domain.SaveTask
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.launch
 import timber.log.Timber
+import java.io.File
 import javax.inject.Inject
 
 class SaveService @Inject constructor(
-    private val embedSubtitleIntoVideoUseCase: EmbedSubtitleIntoVideoUseCase
+    private val embedSubtitleIntoVideoUseCase: EmbedSubtitleIntoVideoUseCase,
+    private val getSyncRecorderDirectoryUseCase: GetSyncRecorderDirectoryUseCase,
+    //private val videoCompressionUseCase: VideoCompressionUseCase,
 ) {
-    private val queue = Channel<SaveTask>(Channel.UNLIMITED)
-    private var serviceStarted = false
+    fun saveOutpuVideo(task: SaveTask) {
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val subtitleFiles = listOf(
+                    "accelerometer_data_${task.recordingCount}.srt",
+                    "gyroscope_data_${task.recordingCount}.srt",
+                    "magnetometer_data_${task.recordingCount}.srt"
+                )
 
-    fun startService(scope: CoroutineScope) {
-        if (serviceStarted) return
-        serviceStarted = true
+                embedSubtitleIntoVideoUseCase(
+                    task.videoName,
+                    subtitleFiles,
+                    task.outputName
+                )
 
-        scope.launch(Dispatchers.IO) {
-            for (task in queue) {
-                try {
-                    embedSubtitleIntoVideoUseCase(
-                        task.videoName,
-                        task.sensorName,
-                        task.outputName
-                    )
-                    Timber.tag(TAG).d("✅ Embedded and saved: ${task.outputName}")
-                } catch (e: Exception) {
-                    Timber.tag(TAG).e(e, "❌ Failed saving: ${task.outputName}")
+                /*val compressedVideoName = "compressed_${task.outputName}"
+                val compressJob = async {
+                    videoCompressionUseCase(task.outputName, compressedVideoName)
                 }
+                compressJob.await()
+
+                deleteOldVideoFiles(task.videoName, task.outputName)*/
+
+                Timber.tag(TAG).d("✅ Embedded and saved: ${task.outputName}")
+            } catch (e: Exception) {
+                Timber.tag(TAG).e(e, "❌ Failed saving: ${task.outputName}")
             }
         }
     }
 
-    suspend fun addTask(task: SaveTask) {
-        Timber.tag(TAG).d("📥 Task added to queue: $task")
-        queue.send(task)
+    // Delete old files
+    private fun deleteOldVideoFiles(vararg files: String) {
+        files.forEach { fileName ->
+            val file = File(getSyncRecorderDirectoryUseCase(), fileName)
+            if (file.exists()) {
+                val deleted = file.delete()
+                if (deleted) {
+                    Timber.tag(TAG).d("✅ File deleted: $fileName")
+                } else {
+                    Timber.tag(TAG).e("❌ Failed to delete file: $fileName")
+                }
+            }
+        }
     }
 }
