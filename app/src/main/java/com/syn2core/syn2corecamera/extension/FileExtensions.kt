3c55bfeaca8b754ff6c90a948e50ac6d168961df
ak <aminkeshavarzian@gmail.com>
2025-06-03 20:04:51 +0330

    extract common downloads directory logic to utility function

--- app/src/main/java/com/syn2core/syn2corecamera/extension/FileExtensions.kt
+++ app/src/main/java/com/syn2core/syn2corecamera/extension/FileExtensions.kt
@@ -6,73 +6,66 @@ import java.io.FileNotFoundException
 import java.io.IOException
 import java.io.RandomAccessFile
 
+fun File.getImuFile(): File = File(
+    getSyn2CoreDownloadsDir(),
+    name.replace(".mp4", "_imu.txt")
+)
 
-fun File.getImuFile(): File {
+fun File.getFramesFile(): File = File(
+    getSyn2CoreDownloadsDir(),
+    name.replace(".mp4", "_ft.txt")
+)
+
+private fun getSyn2CoreDownloadsDir(): File {
     val downloadsDir = File(
         Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),
         "syn2core"
     )
     if (!downloadsDir.exists()) downloadsDir.mkdirs()
-
-    return File(
-        downloadsDir,
-        name.replace(".mp4", "_imu.txt")
-    )
+    return downloadsDir
 }
 
-fun File.getFramesFile(): File {
-    val downloadsDir = File(
-        Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),
-        "syn2core"
-    )
-    if (!downloadsDir.exists()) downloadsDir.mkdirs()
-
-    return File(
-        downloadsDir,
-        name.replace(".mp4", "_ft.txt")
-    )
-}
 
 val File.lastLine: String?
     get() {
-    var fileHandler: RandomAccessFile? = null
-    try {
-        fileHandler = RandomAccessFile(this, "r")
-        val fileLength = fileHandler.length() - 1
-        val sb = StringBuilder()
+        var fileHandler: RandomAccessFile? = null
+        try {
+            fileHandler = RandomAccessFile(this, "r")
+            val fileLength = fileHandler.length() - 1
+            val sb = StringBuilder()
 
-        for (filePointer in fileLength downTo -1 + 1) {
-            fileHandler.seek(filePointer)
-            val readByte = fileHandler.readByte().toInt()
+            for (filePointer in fileLength downTo -1 + 1) {
+                fileHandler.seek(filePointer)
+                val readByte = fileHandler.readByte().toInt()
 
-            if (readByte == 0xA) {
-                if (filePointer == fileLength) {
-                    continue
+                if (readByte == 0xA) {
+                    if (filePointer == fileLength) {
+                        continue
+                    }
+                    break
+                } else if (readByte == 0xD) {
+                    if (filePointer == fileLength - 1) {
+                        continue
+                    }
+                    break
                 }
-                break
-            } else if (readByte == 0xD) {
-                if (filePointer == fileLength - 1) {
-                    continue
-                }
-                break
+
+                sb.append(readByte.toChar())
             }
 
-            sb.append(readByte.toChar())
-        }
-
-        val lastLine = sb.reverse().toString()
-        return lastLine
-    } catch (e: FileNotFoundException) {
-        e.printStackTrace()
-        return null
-    } catch (e: IOException) {
-        e.printStackTrace()
-        return null
-    } finally {
-        if (fileHandler != null) try {
-            fileHandler.close()
+            val lastLine = sb.reverse().toString()
+            return lastLine
+        } catch (e: FileNotFoundException) {
+            e.printStackTrace()
+            return null
         } catch (e: IOException) {
-            /* ignore */
+            e.printStackTrace()
+            return null
+        } finally {
+            if (fileHandler != null) try {
+                fileHandler.close()
+            } catch (e: IOException) {
+                /* ignore */
+            }
         }
     }
-}
